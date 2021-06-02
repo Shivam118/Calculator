fc6280a0f9ae5255b228f1c3182de479e50e361a
Shivam118 <sharmashushant118@gmail.com>
2021-06-02 20:24:36 +0530

    changes made still numbers displaying

--- calculator.js
+++ calculator.js
@@ -1,30 +1,42 @@
-function click1(){
-    document.getElementById("Display").placeholder = '1';    
-    }
-    function click2(){
-    document.getElementById("Display").placeholder = '2';    
-    }
-    function click3(){
-    document.getElementById("Display").placeholder = '3';    
-    }
-    function click4(){
-    document.getElementById("Display").placeholder = '4';    
-    }
-    function click5(){
-    document.getElementById("Display").placeholder = '5';    
-    }
-    function click6(){
-    document.getElementById("Display").placeholder = '6';    
-    }
-    function click7(){
-    document.getElementById("Display").placeholder = '7';    
-    }
-    function click8(){
-    document.getElementById("Display").placeholder = '8';    
-    }
-    function click9(){
-    document.getElementById("Display").placeholder = '9';    
-    }
-    function click0(){
-    document.getElementById("Display").placeholder = '0';    
-    }
+var numbers = [];
+var click_equal = false;
+
+function click_equals(){
+    click_equal = true;
+}
+
+while(click_equal){
+    
+    click_equals();
+
+}
+function click1() {
+    document.getElementById("Display").placeholder = '1';
+}
+function click2() {
+    document.getElementById("Display").placeholder = '2';
+}
+function click3() {
+    document.getElementById("Display").placeholder = '3';
+}
+function click4() {
+    document.getElementById("Display").placeholder = '4';
+}
+function click5() {
+    document.getElementById("Display").placeholder = '5';
+}
+function click6() {
+    document.getElementById("Display").placeholder = '6';
+}
+function click7() {
+    document.getElementById("Display").placeholder = '7';
+}
+function click8() {
+    document.getElementById("Display").placeholder = '8';
+}
+function click9() {
+    document.getElementById("Display").placeholder = '9';
+}
+function click0() {
+    document.getElementById("Display").placeholder = '0';
+}
